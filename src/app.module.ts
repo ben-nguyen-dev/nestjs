a3de677f61067cf52c8ed874ec7c3c52a82b92d1
LinhLD <leduylinh1998.nc@gmail.com>
2022-11-08 00:06:36 +0700

    add prettier, mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot('mongodb+srv://admin:LInhduyle%401998@mongodb.kevqyya.mongodb.net/?retryWrites=true&w=majority')],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
